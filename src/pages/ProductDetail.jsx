02161ea3611a111b67bb886e9bdd38de5859bec4
paulo-dev-fortaleza <paulinhobrito@ymail.com>
2025-06-25 15:08:40 -0300

    segundo commit

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,1 @@
+export default function ProductDetail() { return <div><h1>Detalhe do Produto</h1></div>; }
---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,1 @@
+export default function ProductList() { return <div><h1>Página de Produtos</h1></div>; }
